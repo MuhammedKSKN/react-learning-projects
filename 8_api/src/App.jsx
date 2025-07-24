dabf10c46b914000b7e36a3149d0376445785826
Muhammed Keskin <keskinmuhammed49@gmail.com>
2025-07-24 10:17:31 +0300

    8.ders post tamamlandı

--- 8_api/src/App.jsx
+++ 8_api/src/App.jsx
@@ -19,13 +19,30 @@ function App() {
   //getUserById fonksiyonu, belirli bir kullanıcıyı id ile alır.
   //template literals kullanarak url'yi dinamik hale getirebilirsin js dersler 94
 
+  const createUser = async (user) => {
+    const response = await axios.post(`${BASE_URL}/users`, user);
+    console.log(response.data);
+  }
+  //Linki dinamik hale getirmek için template literals kullanabilirsin.
+  //responsu yakalamasan da olur test için o 
+
 
   //async ve await kullanarak asenkron fonksiyonlar oluşturabiliriz.
   //Bunu js dersleri 108-110. video izleyebilirsin
   useEffect(() => {
-    getAllUsers();
+    //getAllUsers();
+    //getUserById(1);
+
+    const newUser = {
+      name: 'Enes',
+      age: 25,
+      email: 'enes@example.com',
+      city: 'İstanbul'
+    };
+    createUser(newUser);
+
+    // useEffect, component mount olduğunda çalışacak ve getAllUsers fonksiyonunu çağıracak.
   }, []);
-  // useEffect, component mount olduğunda çalışacak ve getAllUsers fonksiyonunu çağıracak.
 
   return (
     <div>
