37d8d8f3f427f9ed55e0462b569b68b1d6483a0b
Muhammed Keskin <keskinmuhammed49@gmail.com>
2025-07-24 09:56:49 +0300

    8.ders Get İsteği Bitti

--- 8_api/src/App.jsx
+++ 8_api/src/App.jsx
@@ -1,7 +1,31 @@
 import { useState } from 'react'
 import './App.css'
+import axios from 'axios'
+import { useEffect } from 'react'
 
 function App() {
+
+  const BASE_URL = 'http://localhost:3001'
+
+  const getAllUsers = async () => {
+    const response = await axios.get(BASE_URL + '/users');
+    console.log(response.data);
+  }
+
+  const getUserById = async (id) => {
+    const response = await axios.get(BASE_URL + '/users/' + id);
+    console.log(response.data);
+  }
+  //getUserById fonksiyonu, belirli bir kullanıcıyı id ile alır.
+  //template literals kullanarak url'yi dinamik hale getirebilirsin js dersler 94
+
+
+  //async ve await kullanarak asenkron fonksiyonlar oluşturabiliriz.
+  //Bunu js dersleri 108-110. video izleyebilirsin
+  useEffect(() => {
+    getAllUsers();
+  }, []);
+  // useEffect, component mount olduğunda çalışacak ve getAllUsers fonksiyonunu çağıracak.
 
   return (
     <div>
